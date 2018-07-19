eee83e1958d9499b7fca738d499d6bbd76580db3
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-07-19 11:05:57 -0400

    added style (+ simple bootstrap grid for responsiveness); tested and fixed bugs; implemented findSunriseSunsetTime by both coordinates and city name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {APIServiceClient} from '../services/APIServiceClient';
-import { } from '@types/googlemaps';
+import {} from '@types/googlemaps';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
 
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
-
+  infoWindow: google.maps.InfoWindow;
 
   city: string;
   lng: number;
@@ -19,17 +19,20 @@ export class HomeComponent implements OnInit {
   location: Object;
   apiData: Object;
 
-  constructor(private apiService: APIServiceClient) {}
+  constructor(private apiService: APIServiceClient) {
+  }
 
   getLocation() {
     this.apiService.getLocation(this.city)
       .then(res => {
-      this.lat = res.results[0].geometry.location.lat;
-      this.lng = res.results[0].geometry.location.lng;
-      console.log(this.lat);
-    })
-      .then(() => this.getData());
+        this.lat = res.results[0].geometry.location.lat;
+        this.lng = res.results[0].geometry.location.lng;
+        this.city = res.results[0].formatted_address;
+        this.setCenter();
+        this.getData();
+      });
   }
+
 
   getData() {
     this.apiService.getAPIData(this.lat, this.lng)
@@ -38,24 +41,48 @@ export class HomeComponent implements OnInit {
       });
   }
 
-  // setMapType(mapTypeId: string) {
-  //   this.map.setMapTypeId(mapTypeId)
-  // }
-
-  setCenter(e:any) {
-    e.preventDefault();
+  setCenter() {
     this.map.setCenter(new google.maps.LatLng(this.lat, this.lng));
+  }
+
+  setCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat =  position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.infoWindow.setPosition(new google.maps.LatLng(this.lat, this.lng));
+        this.infoWindow.setContent('You are here');
+        this.infoWindow.open(this.map);
+        this.setCenter();
+        this.getData();
+      }, function() {
+        this.handleLocationError(true, this.infoWindow, this.map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      this.handleLocationError(false, this.infoWindow, this.map.getCenter());
+    }
+  }
+
+  handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(this.map);
   }
 
   ngOnInit() {
     this.city = '';
-
     const mapProp = {
       center: new google.maps.LatLng(50.4501, 30.5234),
-      zoom: 12,
+      zoom: 10,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    this.infoWindow = new google.maps.InfoWindow;
+    this.setCurrentLocation();
   }
+
 
 }
--- src/app/services/APIServiceClient.ts
+++ src/app/services/APIServiceClient.ts
@@ -4,7 +4,7 @@ import {Injectable} from '@angular/core';
 export class APIServiceClient {
 
   // Sunrise sunset API
-  getAPIData(lat, lng) {
+  getAPIData(lat: number, lng: number) {
     const url = 'https://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + lng + ' &formatted=1'
     return fetch(url).then(response => response.json());
   }
